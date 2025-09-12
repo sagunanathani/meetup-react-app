2b0ac0b661c9d78c2a8b638284ba4d9045d9eef8
Saguna Nathani <sagunanathani@gmail.com>
2025-09-12 16:42:16 +0200

    Install and configure Atatus for React app monitoring
    Remove Atatus test error

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import * as atatus from "atatus-spa";
+// Initialize Atatus with your API key
+atatus.config("0203e190c5fd4587ad6d3a2dee9e9595").install();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// send a test error to confirm Atatus setup
+//atatus.notify(new Error("Test Atatus Setup"));
