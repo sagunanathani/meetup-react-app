e6b85ed434fee6dedcdbc757e67acf15a139da63
Saguna Nathani <sagunanathani@gmail.com>
2025-09-16 14:21:25 +0200

    fix: improve CityEventsChart responsiveness and label handling

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -1,31 +1,27 @@
-import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
 import {
+  ResponsiveContainer,
   ScatterChart,
   Scatter,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
-  ResponsiveContainer,
 } from "recharts";
+import PropTypes from "prop-types";
 
 const CityEventsChart = ({ allLocations, events }) => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    if (!allLocations || !events) return;
-
+  const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter(
         (event) => event.location === location
       ).length;
-      const city = location.split(", ")[0]; // only city name
+      const city = location.split(/, | - /)[0]; // fix long labels
       return { city, count };
     });
+    return data;
+  };
 
-    setData(data);
-  }, [events, allLocations]); // re-run when events/locations change
+  const data = getData();
 
   return (
     <ResponsiveContainer width="99%" height={400}>
@@ -33,12 +29,19 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 20,
           right: 20,
-          bottom: 20,
-          left: 20,
+          bottom: 60, // extra space // was 20, now 60 to make room for labels
+          left: -30, // tighter alignment - remove empty space on left
         }}
       >
         <CartesianGrid />
-        <XAxis type="category" dataKey="city" name="City" />
+        <XAxis
+          type="category"
+          dataKey="city"
+          name="City"
+          angle={60}
+          interval={0}
+          tick={{ dx: 20, dy: 40, fontSize: 14 }}
+        />
         <YAxis type="number" dataKey="count" name="Number of Events" />
         <Tooltip cursor={{ strokeDasharray: "3 3" }} />
         <Scatter name="Events" data={data} fill="#8884d8" />
